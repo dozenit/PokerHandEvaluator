d5dd1fe736b35bc26c89e00666f9a63a7ab22381
dozenit <modery.dev@gmail.com>
2021-04-26 19:05:08 +0200

    Adds poker category.

---
+++ src/test/java/poker/CategoryTest.java
@@ -0,0 +1,17 @@
+package poker;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Locale;
+
+public class CategoryTest {
+
+    static final int POKER_CATEGORIES_COUNT = 9;
+
+    @Test
+    void shouldHaveNineValues() {
+        Assertions.assertEquals(POKER_CATEGORIES_COUNT,
+                Category.values().length);
+    }
+}
